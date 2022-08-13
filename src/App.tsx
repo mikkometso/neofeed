dba4a74f089f82e3c908aa945147af8cff210026
Mikko Metso <mikko.metso@sok.fi>
2022-08-13 14:47:56 +0300

    Clean up info text to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import "./styles.css";
-import ErrorModal from "./components/ErrorModal/ErrorModal";
-import Form from "./components/Form/Form";
 import LoadingBar from "./components/LoadingBar/LoadingBar";
+import ErrorModal from "./components/ErrorModal/ErrorModal";
+import Info from "./components/Info/Info";
+import Form from "./components/Form/Form";
 import { NearEarthObject } from "./types/api.types";
 import { formatDateToYmd } from "./utils/formatDateToYmd";
 import { addDaysToDate } from "./utils/addDaysToDate";
@@ -99,17 +100,7 @@ function App() {
       <LoadingBar isLoading={isLoading} />
       <div className={styles.App}>
         <ErrorModal resStatus={resStatus} />
-        <div className={styles.section}>
-          <h1>The nearest asteroid app</h1>
-          <p>
-            I'm a silly little app that fetches data about the nearest passing
-            asteroid from given dates
-          </p>
-          <p>Pass some dates in to get the data</p>
-          <p>
-            You should input date in format dd.mm.yyyy so for example 01.07.2022
-          </p>
-        </div>
+        <Info />
         <Form
           handleSubmit={handleSubmit}
           handleInput={handleInput}
